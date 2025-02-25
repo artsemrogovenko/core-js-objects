480aabe95314876167615941f235fd7446b15d84
Artem_PC <taftoos2@gmail.com>
2025-02-25 22:24:34 +0300

    feat: solve Rectangle test

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -218,8 +218,10 @@ return result;
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width=width;
+ this.height=height
+  this.getArea =() =>{return this.width*this.height};
 }
 
 /**
